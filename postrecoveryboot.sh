bb9f33a63b3f8f3b783ee6617eb8b30d5a478802
Koushik Dutta <koushd@gmail.com>
2011-01-01 19:53:55 -0800

    fix booting android after recovery boots.
    Change-Id: I97f28eefb1ece56cff334aad7372c247371a9f56

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -1,3 +1,11 @@
 #!/sbin/sh
 
 mount /dev/block/mmcblk0p7 /cache
+
+mkdir /rom
+mount /dev/block/mmcblk0p2 /rom
+echo -n -e "\x00\x00\x00\x00" > /rom/devconf/BootCnt 
+echo -n -e "\x00\x00\x00\x00" > /rom/bcb
+umount /rom
+rmdir /rom
+
