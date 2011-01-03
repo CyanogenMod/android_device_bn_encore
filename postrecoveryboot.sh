2cdff4f46bb1c7dc928ec521a76eccc6d1149b4a
Robert Garcia <rjgarcia@gmail.com>
2011-01-02 22:50:07 -0600

    add dirs for custom mountpoints

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -2,10 +2,12 @@
 
 mount /dev/block/mmcblk0p7 /cache
 
+# make dirs for custom mounts
 mkdir /rom
+mkdir /factory
+
+# clear recovery flags and rest boot count for u-boot
 mount /dev/block/mmcblk0p2 /rom
 echo -n -e "\x00\x00\x00\x00" > /rom/devconf/BootCnt 
 echo -n -e "\x00\x00\x00\x00" > /rom/bcb
 umount /rom
-rmdir /rom
-
