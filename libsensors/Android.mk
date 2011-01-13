ccbce5ce3eb61c0cbe79458365e0e07c2e08a2c0
Brandon Bennett <bennetb@gmail.com>
2011-01-13 00:21:58 -0700

    Fix some compliation problems as well as add a comment about what we are doing in postrecovery.sh
    Change-Id: I5140287dc821377f42b696a2ee2e7b7c9cbfe840

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -14,19 +14,27 @@
 
 
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+
+ifneq ($(TARGET_SIMULATOR),true)
 
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := sensors.nookcolor
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := sensors.c
 
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SHARED_LIBRARIES := libcutils
 
-
 include $(BUILD_SHARED_LIBRARY)
+
+endif # !TARGET_SIMULATOR
+
