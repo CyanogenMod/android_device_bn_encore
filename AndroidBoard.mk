ac60d234ae646a8ad13c262fb1770a7a0d84cb91
Brandon Bennett <bennetb@gmail.com>
2011-02-20 20:16:59 -0700

    Fix reseting the bcb for stock 1.1.0 uboot.bin
    The old way of writing four block of zeros to /rom/bcb doesn't work with the new
    u-boot.bin included with the stock 1.1.0 update which results in the device
    always reboot into recovery.
    
    This writes 1088 zeros to /rom/bcb which was copied from the romrestore.zip also
    included with the 1.1.0 stock update.
    
    Change-Id: Ic174f273a4dc27a0351212a27ca6b697a30a3649

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -6,7 +6,7 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/postrecoveryboot.sh
 ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/postrecoveryboot.sh | $(ACP)
+$(file) : $(LOCAL_PATH)/recovery/postrecoveryboot.sh | $(ACP)
 	$(transform-prebuilt-to-target)
 
 # include the non-open-source counterpart to this file
