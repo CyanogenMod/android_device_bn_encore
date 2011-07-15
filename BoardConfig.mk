7d4267c92f1168604f2b335fb9c31eb544d65997
Dalingrin <dalingrin@gmail.com>
2011-07-15 16:53:21 -0500

    Encore: Enable use of TLS register
    Change-Id: I9197393df591f69497aba40284a766a347206324

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,7 @@ TARGET_BOARD_PLATFORM := omap3
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp 
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp 
 TARGET_BOOTLOADER_BOARD_NAME := encore
