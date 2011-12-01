825a80c750169a0f36545be6fafbdf3ebdb0eea9
fattire <fattire@laptop.(none)>
2011-12-01 01:40:51 -0800

    minor tweaks.  Untested as of yet.  If it breaks, this may be why.  Or it may be something else.

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -20,22 +20,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { NULL };
-
-char* MENU_ITEMS[] = { "reboot system now",
-                       "apply update from sdcard",
-                       "wipe data/factory reset",
-                       "wipe cache partition",
-                       "install zip from sdcard",
-                       "backup and restore",
-                       "mounts and storage",
-                       "advanced",
-                       NULL };
-
-int device_recovery_start() {
-    return 0;
-}
-
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -47,9 +31,6 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
     return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
-int device_reboot_now(volatile char* key_pressed, int key_code) {
-    return 0;
-}
 
 int device_handle_key(int key_code, int visible) {
     if (visible) {
@@ -87,12 +68,4 @@ int device_handle_key(int key_code, int visible) {
     }
 
     return NO_ACTION;
-}
-
-int device_perform_action(int which) {
-    return which;
-}
-
-int device_wipe_data() {
-    return 0;
 }
