343fb9f89a51b18beb46ec930e5ab31c01d52881
ryands <ryan@ryands.org>
2011-12-04 01:28:20 -0500

    Stop ramdisk_tools from updating init for internal
    Change-Id: I3f896c939d585e299695c39016d89c76f48cd4a8

--- ramdisk_tools.sh
+++ ramdisk_tools.sh
@@ -165,7 +165,6 @@ case $1 in
 --internal)
     # rebuild ramdisk for use on standard emmc partitions
     check_mkimage
-    update_init $INT_SYSTEM $INT_USERDATA $INT_CACHE
     create_ramdisk $INT_OUT
     cleanup_mess
     output_info $INT_NAME $INT_BOOT $INT_OUT $INT_RAMDISK_NAME
