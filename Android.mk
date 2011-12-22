21d4755ddc9d8a5b5c7aeecb4f3d9d1888888e39
fattire <fattire@indirect.com>
2011-12-22 14:28:10 -0700

    update Android.mk to match other devices for ics
    Change-Id: I205abc8e14bd0340dd8d5edc0ed2985de1243f17

--- Android.mk
+++ Android.mk
@@ -1,6 +1,4 @@
-# Copyright (C) 2010 The Android Open Source Project
-# Copyright (C) 2011 fattire
-# Copyright (C) 2011 Gerad Munsch <gmunsch@unforgivendevelopment.com>
+# Copyright (C) 2011 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+# WARNING: Everything listed here will be built on ALL platforms,
+# including x86, the emulator, and the SDK.  Modules must be uniquely
+# named (liblights.encore), and must build everywhere, or limit themselves
+# to only building on ARM if they include assembly. Individual makefiles
+# are responsible for having their own logic, for fine-grained control.
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter encore,$(TARGET_DEVICE)),)
+# if some modules are built directly from this directory (not subdirectories),
+# their rules should be written here.
 
-ifneq ($(TARGET_SIMULATOR),true)
-include $(call first-makefiles-under,$(LOCAL_PATH))
-endif
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
-endif
