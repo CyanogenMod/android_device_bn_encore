a4020d3dac38cf3db2fa66f7f85089f5c034ff40
fattire <fattire@indirect.com>
2011-12-22 14:26:47 -0700

    new (experimental) fingerprint
    Change-Id: Iddb710ebfdfa5f70b83c9d6d391bd19fa746eb1f

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ PRODUCT_RELEASE_NAME := NookColor
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=encore BUILD_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys" BUILD_NUMBER=189904
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=encore BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="bn/bn_encore/encore:2.3.4/IML74K/228551:user/release-keys"  PRIVATE_BUILD_DESC="encore-user 4.0.3 IML74K 228551 release-keys"
 
 PRODUCT_NAME := cm_encore
 PRODUCT_DEVICE := encore
