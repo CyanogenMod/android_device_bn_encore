f0f7169b8192aecde6eb285d12567c4176c46c6f
fattire <fattire@indirect.com>
2012-01-01 19:56:10 -0800

    Add AndroidBoard.mk until we figure out the right place for this.
    It adds stuff for recovery.
    
    Change-Id: I6257cbc30f3524c8328db914ee542d18cb0e1ea4

---
+++ AndroidBoard.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# AndroidBoard.mk is a legacy mechanism to deal with a few
+# edge-cases that can't be managed otherwise. No new rules
+# should be added to this file.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/postrecoveryboot.sh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/postrecoveryboot.sh | $(ACP)
+	$(transform-prebuilt-to-target)
+
+# include the non-open-source counterpart to this file
+-include vendor/bn/encore/AndroidBoardVendor.mk
+
