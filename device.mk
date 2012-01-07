01ac8f2518026810a7b9aa73aeb35cb50d10a94f
fattire <fattire@indirect.com>
2012-01-07 03:01:59 -0800

    enable usb host - works for keyboard/mouse (confirmed) - not block device yet
    Thanks to DJ-Gutz for suggesting and drakmors for testing
    
    Change-Id: Ic291fbfcc5d226e29148b2bcb6eaab828e93a1a5

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
