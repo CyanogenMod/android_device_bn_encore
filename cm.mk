80156d03ed7c6fe0df6727df9cdfd1181c7cf305
fattire <fattire@laptop.(none)>
2012-03-18 12:10:05 -0700

    add target_bootanimation setting for our resolution + update fingerprint to 4.0.3 from 2.3.4
    ...hopefully not breaking anything.

--- cm.mk
+++ cm.mk
@@ -20,7 +20,8 @@ PRODUCT_RELEASE_NAME := NookColor
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=encore BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="bn/bn_encore/encore:2.3.4/IML74K/228551:user/release-keys"  PRIVATE_BUILD_DESC="encore-user 4.0.3 IML74K 228551 release-keys"
+TARGET_BOOTANIMATION_NAME := horizontal-1024x768
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=encore BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="bn/bn_encore/encore:4.0.3/IML74K/228551:user/release-keys" PRIVATE_BUILD_DESC="encore-user 4.0.3 IML74K 228551 release-keys"
 
 PRODUCT_NAME := cm_encore
 PRODUCT_DEVICE := encore
