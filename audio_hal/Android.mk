a04a7b7af2dec9c0e675a5b0f3856386d1fec564
Steven Luo <steven@steven676.net>
2012-07-21 18:19:19 -0700

    Build audio.primary.encore.so HAL module
    No code needed -- we just need to link the appropriate bits together.
    Makefile fragment from hardware/cm/audio/Android.mk.

--- audio_hal/Android.mk
+++ audio_hal/Android.mk
@@ -69,5 +69,34 @@ ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 
   include $(BUILD_SHARED_LIBRARY)
 
+# Build the audio.primary HAL which will be used by audioflinger to interface
+# with the rest of the ALSA audio stack
+# Makefile fragment comes from hardware/cm/audio/Android.mk
+# Note that we don't actually need any extra code!
+
+  include $(CLEAR_VARS)
+
+  LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
+  LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+  LOCAL_MODULE_TAGS := optional
+
+  LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia \
+    libhardware_legacy
+
+  LOCAL_SHARED_LIBRARIES += libdl
+
+  LOCAL_SHARED_LIBRARIES += libaudio
+
+  LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+  LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiohw_legacy
+
+  include $(BUILD_SHARED_LIBRARY)
+
 endif
 endif
