70e34930fa5cc6c5f9c6c4d76066e05e7424dbda
Krylon360 <b.t.walter@gmail.com>
2012-07-23 06:01:32 -0600

    Wifi fixes and PVR modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ TARGET_KERNEL_CONFIG := encore_defconfig
 TARGET_KERNEL_SOURCE := kernel/bn/encore
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 # Fallback prebuilt kernel
-TARGET_PREBUILT_KERNEL := device/bn/encore/prebuilt/boot/kernel
+#TARGET_PREBUILT_KERNEL := device/bn/encore/prebuilt/boot/kernel
 
 # Connectivity - Wi-Fi
 USES_TI_MAC80211 := true
@@ -92,8 +92,10 @@ BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE		 := wl12xx_mac80211
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER		 := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 endif
 
@@ -109,7 +111,12 @@ WIFI_MODULES:
 	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
 	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
 
-TARGET_KERNEL_MODULES := WIFI_MODULES
+SGX_MODULES:
+	cp kernel/bn/encore/drivers/video/omap2/omapfb/omapfb.h $(KERNEL_OUT)/drivers/video/omap2/omapfb/omapfb.h
+	cp device/bn/encore/prebuilt/GFX/system/lib/modules/omaplfb.ko $(KERNEL_MODULES_OUT)
+	cp device/bn/encore/prebuilt/GFX/system/lib/modules/pvrsrvkm.ko $(KERNEL_MODULES_OUT)
+
+TARGET_KERNEL_MODULES := SGX_MODULES WIFI_MODULES
 
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_RECOVERY_IGNORE_BOOTABLES := true
@@ -120,7 +127,7 @@ TARGET_RECOVERY_PRE_COMMAND := "dd if=/dev/zero of=/rom/bcb bs=64 count=1 > /dev
 TARGET_NO_RADIOIMAGE := true
 
 # HW Graphics (EGL fixes + webkit fix)
-# USE_OPENGL_RENDERER := true
+USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/bn/encore/egl.cfg
 #COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE \
 #			-DMISSING_EGL_PIXEL_FORMAT_YV12 \
@@ -153,7 +160,7 @@ BOARD_HAS_NO_MISC_PARTITION := true
 # TARGET_PROVIDES_LIBAUDIO := true
 # BOARD_USES_GENERIC_AUDIO := false
 # BOARD_USES_ALSA_AUDIO := true
-# BUILD_WITH_ALSA_UTILS := true
+BUILD_WITH_ALSA_UTILS := true
 
 HARDWARE_OMX := true
 
