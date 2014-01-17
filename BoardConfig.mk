297e6070988df501d30ef22efdf291f20bb9c8ff
David Marble <drmarble1@gmail.com>
2014-01-16 21:19:05 -0600

    Use TI wpa supplicant.
    This allows us to view the MAC address in Settings
    and reconnect wifi after sleep.
    
    Change-Id: I7d56f4987f1ff3e46b7fa6d2051035b701e34b69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,7 +108,7 @@ TARGET_KERNEL_SOURCE := kernel/bn/encore
 USES_TI_MAC80211 := true
 ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
@@ -116,6 +116,7 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
+BOARD_WIFI_SKIP_CAPABILITIES     := true
 endif
 
 TARGET_MODULES_SOURCE := "hardware/ti/wlan/mac80211/compat_wl12xx"
