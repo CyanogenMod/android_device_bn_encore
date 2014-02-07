7d8dbbccf1209017ea40a1fb0fdff2ff30780fdf
Steven Luo <steven@steven676.net>
2014-02-06 23:24:15 -0800

    Make the recovery boot-location-agnostic
    Instead of using fixed block device names for /system and /data, let
    fsfinder set up links for us based on the boot location.  This allows us
    to run unmodified from both eMMC and external SD card and to do the
    right thing in both cases.

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -12,5 +12,7 @@ dd if=/dev/zero of=/rom/devconf/BootCnt bs=1 count=4
 dd if=/dev/zero of=/rom/bcb bs=1 count=1088
 
 umount /rom
-rmdir /rom
 
+# Set up device symlinks based on the boot location
+mkdir -p /dev/block/by-name
+/sbin/fsfinder
