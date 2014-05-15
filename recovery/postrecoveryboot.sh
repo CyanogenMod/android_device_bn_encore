f69e40edf1f2e881e5573bcf86dc127a34408cc9
Steven Luo <steven@steven676.net>
2014-05-15 00:44:24 -0700

    recovery: fix reboot-to-recovery from within recovery
    Fix two problems preventing reboot-to-recovery from working in recovery:
    
    * /rom wasn't necessarily mounted before trying to write to /rom/bcb
    * /system/bin was empty, so the commands in the shell script fragment
      weren't found
    
    Change-Id: If30a87caa62190804e78ca76370856d427e0ea5a

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -12,3 +12,8 @@ dd if=/dev/zero of=/rom/devconf/BootCnt bs=1 count=4
 dd if=/dev/zero of=/rom/bcb bs=1 count=1088
 
 umount /rom
+
+# Symlink /system/bin to /sbin for compatibility with the reboot-recovery
+# script and other shell scripts for the regular, non-recovery environment
+rmdir /system/bin
+ln -s /sbin /system/bin
